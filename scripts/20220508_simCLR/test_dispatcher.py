dc9ed1913fb1d4aa8646a385ab7ade022e704e87
RichieHakim <RichHakim@gmail.com>
2022-05-11 02:25:06 -0400

    debugging

--- scripts/20220508_simCLR/test_dispatcher.py
+++ scripts/20220508_simCLR/test_dispatcher.py
@@ -148,37 +148,55 @@ with open(str(Path(dir_save) / 'parameters_batch.json'), 'w') as f:
 
 ## define slurm SBATCH parameters
 sbatch_config_default = \
-f"""#!/usr/bin/bash
-#SBATCH --job-name=simCLR_test
+f"""
+#!/usr/bin/zsh
+#SBATCH --job-name=jupyter
 #SBATCH --output={path_log}
-#SBATCH --partition=gpu_quad
-#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --partition=priority
 #SBATCH -c 2
 #SBATCH -n 1
-#SBATCH --mem=2GB
+#SBATCH --mem=100GB
 #SBATCH --time=0-00:00:10
-
+date;hostname;pwd
 unset XDG_RUNTIME_DIR
+port=$(shuf -i 50000-60000 -n 1)
 
-cd /n/data1/hms/neurobio/sabatini/rich/
-
-echo "loading modules"
-module load gcc/9.2.0 cuda/11.2 conda2
-
-echo "activating environment"
-eval "$($(which conda) 'shell.bash' 'hook')"
-conda activate ROI_env
-
-echo "starting job"
-python "$@"
+cd /n/groups/datta/
+module load gcc/9.2.0 conda2
+source activate jupyter_launcher
+date
 """
 
-# conda init bash
+# f"""#!/usr/bin/bash
+# #SBATCH --job-name=simCLR_test
+# #SBATCH --output={path_log}
+# #SBATCH --partition=gpu_quad
+# #SBATCH --gres=gpu:rtx8000:1
+# #SBATCH -c 2
+# #SBATCH -n 1
+# #SBATCH --mem=2GB
+# #SBATCH --time=0-00:00:10
 
-# date;hostname;pwd
-# unset XDG_RUNTIME_DIR
 
-# source activate ROI_env
+# cd /n/data1/hms/neurobio/sabatini/rich/
+
+# echo "loading modules"
+# module load gcc/9.2.0 cuda/11.2 conda2
+
+# echo "activating environment"
+# eval "$($(which conda) 'shell.bash' 'hook')"
+# conda activate ROI_env
+
+# echo "starting job"
+# python "$@"
+# """
+
+# # conda init bash
+
+# # date;hostname;pwd
+# # unset XDG_RUNTIME_DIR
+
+# # source activate ROI_env
 
 
 ## run batch_run function
