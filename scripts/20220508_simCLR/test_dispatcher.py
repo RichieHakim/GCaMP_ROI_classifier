e1538473e53450e9ad01a4a2dad285a0ee827e73
RichieHakim <RichHakim@gmail.com>
2022-05-11 01:55:45 -0400

    debugging

--- scripts/20220508_simCLR/test_dispatcher.py
+++ scripts/20220508_simCLR/test_dispatcher.py
@@ -118,7 +118,7 @@ params_template = {
 
 ## make params dicts with grid swept values
 params = copy.deepcopy(params_template)
-params = [container_helpers.deep_update_dict(params, ['temperature'], val) for val in [0.03, 0.1, 0.3,]]
+params = [container_helpers.deep_update_dict(params, ['temperature'], val) for val in [0.03]]
 # params = container_helpers.flatten_list([[container_helpers.deep_update_dict(p, ['lr'], val) for val in [0.00001, 0.0001, 0.001]] for p in params])
 
 params_unchanging, params_changing = container_helpers.find_differences_across_dictionaries(params)
@@ -148,7 +148,7 @@ with open(str(Path(dir_save) / 'parameters_batch.json'), 'w') as f:
 
 ## define slurm SBATCH parameters
 sbatch_config_default = \
-f"""#!/usr/bin/bash
+f"""#!/usr/bin/zsh
 #SBATCH --job-name=simCLR_test
 #SBATCH --output={path_log}
 #SBATCH --partition=gpu_quad
