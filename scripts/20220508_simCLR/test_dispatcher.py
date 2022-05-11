3fa344537c5dd6c8319b6959be480fef3e75e9e1
RichieHakim <RichHakim@gmail.com>
2022-05-11 03:09:01 -0400

    debugging

--- scripts/20220508_simCLR/test_dispatcher.py
+++ scripts/20220508_simCLR/test_dispatcher.py
@@ -148,7 +148,7 @@ with open(str(Path(dir_save) / 'parameters_batch.json'), 'w') as f:
 
 ## define slurm SBATCH parameters
 sbatch_config_default = \
-f"""#!/usr/bin/bash
+f"""#!/usr/bin/zsh
 #SBATCH --job-name=jupyter
 #SBATCH --output={path_log}
 #SBATCH --partition=priority
